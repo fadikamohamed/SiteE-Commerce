a1bdee969902bfc7d339606d5a7493fe96865dea
fadikamohamed <fadika.mohamed02@gmail.com>
2018-07-05 21:07:46 +0200

    ajout code js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
-//Jupes 456456456
+//chemisiers
+womenBlouse = [
+  { ref: 'wb1', image: 'assets/img/femme/chemisiers/chemisier1.jpg', name: 'Chemise bras nu ', price: '19,99' },
+  { ref: 'wb2', image: 'assets/img/femme/chemisiers/chemisier2.jpg', name: 'Chemise rouge longue manche', price: '29,99' },
+  { ref: 'wb3', image: 'assets/img/femme/chemisiers/chemisier3.jpg', name: 'Chemise bleu manche courte', price: '29,99' },
+  { ref: 'wb4', image: 'assets/img/femme/chemisiers/chemisier4.jpg', name: 'Chemise courte blanche', price: '20' },
+  { ref: 'wb5', image: 'assets/img/femme/chemisiers/chemisier5.jpg', name: 'chemise bleu col haut', price: '19,99' }
+];
+//Jeans
+womenJeans = [
+  { ref: 'wj1', image: 'assets/img/femme/jean/jean1.jpg', name: 'Jean avec ourlets', price: '29,99' },
+  { ref: 'wj2', image: 'assets/img/femme/jean/jean2.jpg', name: 'Jean slim gris', price: '29,99' },
+  { ref: 'wj3', image: 'assets/img/femme/jean/jean3.jpg', name: 'Jean bleu', price: '25,99' },
+  { ref: 'wj4', image: 'assets/img/femme/jean/jean4.jpg', name: 'Jean bleu délavé', price: '35,99' },
+  { ref: 'wj5', image: 'assets/img/femme/jean/jean5.jpg', name: 'Jeans stretch fleuri', price: '35,99' }
+];
+//Jupes
   womenSkirt = [
 
     { ref: 'ws1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'Jupe à rayures', price: '30' },
@@ -7,84 +23,71 @@
     { ref: 'ws4', image: 'assets/img/femme/jupe/jupe4.jpg', name: 'Jupe en Jean', price: '29,99' },
     { ref: 'ws5', image: 'assets/img/femme/jupe/jupe5.jpg', name: 'Jupe jaune avec poches', price: '29,99' }
   ];
-//Jeans
-  womenJeans = [
-    { ref: 'wj1', image: 'assets/img/femme/jupe/jean1.jpg', name: 'Jean avec ourlets', price: '29,99' },
-    { ref: 'wj2', image: 'assets/img/femme/jupe/jean2.jpg', name: 'Jean slim gris', price: '29,99' },
-    { ref: 'wj3', image: 'assets/img/femme/jupe/jean3.jpg', name: 'Jean bleu', price: '25,99' },
-    { ref: 'wj4', image: 'assets/img/femme/jupe/jean4.jpg', name: 'Jean bleu délavé', price: '35,99' },
-    { ref: 'wj5', image: 'assets/img/femme/jupe/jean5.jpg', name: 'Jeans stretch fleuri', price: '35,99' }
-  ];
-//Pantalons   45645
+//Pantalons
   womenPants = [
     { ref: 'wt1', image: 'assets/img/femme/pantalon/pantalon1.jpg', name: 'Pantalon blanc à poches', price: '29,99' },
     { ref: 'wt2', image: 'assets/img/femme/pantalon/pantalon2.jpg', name: 'Pantalon slim rose', price: '29,99' },
-    { ref: 'wt3', image: 'assets/img/femme/pantaon/pantalon3.jpg', name: 'Pantalon bleu nuit', price: '25' },
+    { ref: 'wt3', image: 'assets/img/femme/pantalon/pantalon3.jpg', name: 'Pantalon bleu nuit', price: '25' },
     { ref: 'wt4', image: 'assets/img/femme/pantalon/pantalon4.jpg', name: 'pantacourt', price: '19,99' }
   ];
-//chemisiers
-  womenblouse = [
-    { ref: 'wb1', image: 'assets/img/femme/chemisiers/chemisier1.jpg', name: 'Chemise bras nu ', price: '19,99' },
-    { ref: 'wb2', image: 'assets/img/femme/chemisiers/chemisier2.jpg', name: 'Chemise rouge longue manche', price: '29,99' },
-    { ref: 'wb3', image: 'assets/img/femme/chemisiers/chemisier3.jpg', name: 'Chemise bleu manche courte', price: '29,99' },
-    { ref: 'wb4', image: 'assets/img/femme/chemisiers/chemisier4.jpg', name: 'Chemise courte blanche', price: '20' },
-    { ref: 'wb5', image: 'assets/img/femme/chemisiers/chemisier5.jpg', name: 'chemise bleu col haut', price: '19,99' }
-  ];
-
-  coat = [
+//Manteaux
+  womenCoats = [
     { ref: 'wc1', image: 'assets/img/femme/manteau_trench/manteau1.jpg', name: 'Manteau bleu', price: '39,99' },
     { ref: 'wc2', image: 'assets/img/femme/manteau_trench/manteau2.jpg', name: 'Trench en jean', price: '59,99' },
     { ref: 'wc3', image: 'assets/img/femme/manteau_trench/manteau3.jpg', name: 'Trench rose', price: '39,99' },
     { ref: 'wc4', image: 'assets/img/femme/manteau_trench/manteau4.jpg', name: 'Trench Fleuri', price: '39,99' },
     { ref: 'wc5', image: 'assets/img/femme/manteau_trench/manteau5.jpg', name: 'Trench bleu cintré', price: '49,99' }
   ];
-
+//Tshirts
   womenTshirts = [
     { ref: 'wts1', image: 'assets/img/femme/tee_shirt_maillot/Tshirt1.jpg', name: 'Tshirts noir', price: '19,99' },
     { ref: 'wts2', image: 'assets/img/femme/tee_shirt_maillot/Tshirt2.jpg', name: 'Tshirts blanc', price: '9,99' },
     { ref: 'wts3', image: 'assets/img/femme/tee_shirt_maillot/Tshirt3.jpg', name: 'Tshirts rayé', price: '9,99' }
   ];
-
-  pull = [
+//Pulls
+  womenPullover = [
     { ref: 'wp1', image: 'assets/img/femme/pull_gilet_sweat/pull1.jpg', name: 'sweat rose', price: '29,99' },
     { ref: 'wp2', image: 'assets/img/femme/pull_gilet_sweat/pull2.jpg', name: 'gilet style marin', price: '29,99' },
     { ref: 'wp3', image: 'assets/img/femme/pull_gilet_sweat/pull3.jpg', name: 'pull col v blanc', price: '19,99' },
     { ref: 'wp4', image: 'assets/img/femme/pull_gilet_sweat/pull4.jpg', name: 'Pull noir', price: '19,99' }
   ];
 
-/*//Hommes
-var men = [
+//Homme
 
-   pantalon = [
-    { ref: 'mp1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'Pantalon noir', price: '29,99€' },
-    { ref: 'mp2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'Pantalon cintré', price: '25,99€' },
-    { ref: 'mp3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'Pantalon gris', price: '29,99€' }
-  ],
-   Jeans = [
-    { ref: 'mj1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'jean gris', price: '29,99€' },
-    { ref: 'mj2', image: 'assets/img/femme/jupe/jupe5.jpg', name: 'jean rose', price: '29,99€' },
-    { ref: 'mj3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'jean noir', price: '25€' },
-    { ref: 'mj4', image: 'assets/img/femme/jupe/jupe4.jpg', name: 'jean bleu', price: '39,99€' }
+//Pantalons homme
+  menPants = [
+    { ref: 'mp1', image: 'assets/img/homme/pantalon/pantalon1.jpg', name: 'Pantalon noir', price: '29,99' },
+    { ref: 'mp2', image: 'assets/img/homme/pantalon/pantalon2.jpg', name: 'Pantalon cintré', price: '25,99' },
+    { ref: 'mp3', image: 'assets/img/homme/pantalon/pantalon3.jpg', name: 'Pantalon gris', price: '29,99' }
+  ];
+//Jeans homme
+   menJeans = [
+    { ref: 'mj1', image: 'assets/img/homme/jean/jupe1.jpg', name: 'jean gris', price: '29,99' },
+    { ref: 'mj2', image: 'assets/img/homme/jean/jupe5.jpg', name: 'jean rose', price: '29,99' },
+    { ref: 'mj3', image: 'assets/img/homme/jean/jupe3.jpg', name: 'jean noir', price: '25' },
+    { ref: 'mj4', image: 'assets/img/homme/jean/jupe4.jpg', name: 'jean bleu', price: '39,99' }
   ],
    veste = [
-    { ref: 'mv1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'veste en jean', price: '29,99€' },
-    { ref: 'mv2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'veste kaki', price: '29,99€' },
-    { ref: 'mv3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'veste bleu nuit', price: '39,99€' }
+    { ref: 'mv1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'veste en jean', price: '29,99' },
+    { ref: 'mv2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'veste kaki', price: '29,99' },
+    { ref: 'mv3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'veste bleu nuit', price: '39,99' }
   ],
    tshirts = [
-    { ref: 'mt1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'tshirts bleu', price: '9,99€' },
-    { ref: 'mt2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'shirts gris', price: '9,99€' },
-    { ref: 'mt3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'tshirts bleu', price: '9,99€' }
+    { ref: 'mt1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'tshirts bleu', price: '9,99' },
+    { ref: 'mt2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'shirts gris', price: '9,99' },
+    { ref: 'mt3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'tshirts bleu', price: '9,99' }
   ],
+//Chemises homme
   chemise = [
-    { ref: 'mc1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'chemise à carreaux manche courte', price: '15,99€' },
-    { ref: 'mc2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'chemise à carreaux', price: '15,99€' },
-    { ref: 'mc3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'chemise rose', price: '19,99€' }
+    { ref: 'mc1', image: 'assets/img/homme/Chemise/chemise1.jpg', name: 'chemise à carreaux manche courte', price: '15,99' },
+    { ref: 'mc2', image: 'assets/img/homme/Chemise/chemise2.jpg', name: 'chemise à carreaux', price: '15,99' },
+    { ref: 'mc3', image: 'assets/img/homme/Chemise/chemise3.jpg', name: 'chemise bleu', price: '15,99' }
+    { ref: 'mc3', image: 'assets/img/homme/Chemise/chemise4.jpg', name: 'chemise rose', price: '19,99' }
   ],
   pull = [
-    { ref: 'mp1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'pull bleu', price: '29,99€' },
-    { ref: 'mp2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'pull gris à motif', price: '25,99€' },
-    { ref: 'mp3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'pull bleu et vert', price: '29,99€' }
+    { ref: 'mp1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'pull bleu', price: '29,99' },
+    { ref: 'mp2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'pull gris à motif', price: '25,99' },
+    { ref: 'mp3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'pull bleu et vert', price: '29,99' }
   ],
 ];
 
@@ -92,16 +95,16 @@ var men = [
 var girl = [
 
   Robes: [
-    { ref: 'gr1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'robe en jean', price: '9,99€' },
-    { ref: 'gr2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'robe prune', price: '5,99€' },
-    { ref: 'gr3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'robe jaune poussin', price: '12,99€' }
+    { ref: 'gr1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'robe en jean', price: '9,99' },
+    { ref: 'gr2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'robe prune', price: '5,99' },
+    { ref: 'gr3', image: 'assets/img/femme/jupe/jupe3.jpg', name: 'robe jaune poussin', price: '12,99' }
   ],
   jupes = [
-    { ref: 'gj1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'jupe bleu', price: '6,99€' },
-    { ref: 'gj2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'jupe rose', price: '9,99€' }
+    { ref: 'gj1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'jupe bleu', price: '6,99' },
+    { ref: 'gj2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'jupe rose', price: '9,99' }
   ],
   pantalon = [
-    { ref: 'gpt1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'pantalon rose', price: '10,99€' },
+    { ref: 'gpt1', image: 'assets/img/femme/jupe/jupe1.jpg', name: 'pantalon rose', price: '10,99' },
     { ref: 'gpt2', image: 'assets/img/femme/jupe/jupe2.jpg', name: 'pantalon gris', price: '12,99€' }
   ],
   tshirts = [
@@ -141,11 +144,70 @@ var boy = [
 ];*/
 
 $(function() {
+  //Chemisier femme
+  console.log(womenBlouse[0].name);
+  for (var i = 0; i < womenBlouse.length; i++) {
+    $('#womenBlouse').append('<div class="article"><img src="' + womenBlouse[i].image + '" />\n<p class="articleName">' + womenBlouse[i].name + ' | ref: ' + womenBlouse[i].ref +'</p>\n<p class="articleName">' + womenBlouse[i].price + ' euros</p><button type="button" id="button' + womenBlouse[i].ref + '">Ajouter au panier !</button></div>');
+  }
+  //Jeans femme
+  console.log(womenJeans[0].name);
+  for (var i = 0; i < womenJeans.length; i++) {
+    $('#womenJeans').append('<div class="article"><img src="' + womenJeans[i].image + '" />\n<p class="articleName">' + womenJeans[i].name + ' | ref: ' + womenJeans[i].ref +'</p>\n<p class="articleName">' + womenJeans[i].price + ' euros</p><button type="button" id="button' + womenJeans[i].ref + '">Ajouter au panier !</button></div>');
+  }
+//Jupes femme
  console.log(womenSkirt[0].name);
  for (var i = 0; i < womenSkirt.length; i++) {
-   $('#womenSkirt').append('<div class="article"><img src="' + womenSkirt[i].image + '" />\n<p class="articleName">' + womenSkirt[i].name + '</p>\n<p class="articleName">' + womenSkirt[i].price + ' euros</p><button>Mettre dans le panier !</button></div>');
+   $('#womenSkirt').append('<div class="article"><img src="' + womenSkirt[i].image + '" />\n<p class="articleName">' + womenSkirt[i].name + ' | ref: ' + womenSkirt[i].ref +'</p>\n<p class="articleName">' + womenSkirt[i].price + ' euros</p><button type="button" id="button' + womenSkirt[i].ref + '">Ajouter au panier !</button></div>');
+ }
+ //Pantalons femme
+ console.log(womenPants[0].name);
+ for (var i = 0; i < womenPants.length; i++) {
+   $('#womenPants').append('<div class="article"><img src="' + womenPants[i].image + '" />\n<p class="articleName">' + womenPants[i].name + ' | ref: ' + womenPants[i].ref +'</p>\n<p class="articleName">' + womenPants[i].price + ' euros</p><button type="button" id="button' + womenPants[i].ref + '">Ajouter au panier !</button></div>');
+ }
+ //Pulls femme
+ console.log(womenPullover[0].name);
+ for (var i = 0; i < womenPullover.length; i++) {
+   $('#womenPullover').append('<div class="article"><img src="' + womenPullover[i].image + '" />\n<p class="articleName">' + womenPullover[i].name + ' | ref: ' + womenPullover[i].ref +'</p>\n<p class="articleName">' + womenPullover[i].price + ' euros</p><button type="button" id="button' + womenPullover[i].ref + '">Ajouter au panier !</button></div>');
+ }
+ //Manteaux femme
+ console.log(womenCoats[0].name);
+ for (var i = 0; i < womenCoats.length; i++) {
+   $('#womenCoats').append('<div class="article"><img src="' + womenCoats[i].image + '" />\n<p class="articleName">' + womenCoats[i].name + ' | ref: ' + womenCoats[i].ref +'</p>\n<p class="articleName">' + womenCoats[i].price + ' euros</p><button type="button" id="button' + womenCoats[i].ref + '">Ajouter au panier !</button></div>');
+ }
+ //Tshirts femme
+ console.log(womenTshirts[0].name);
+ for (var i = 0; i < womenTshirts.length; i++) {
+   $('#womenTshirts').append('<div class="article"><img src="' + womenTshirts[i].image + '" />\n<p class="articleName">' + womenTshirts[i].name + ' | ref: ' + womenTshirts[i].ref +'</p>\n<p class="articleName">' + womenTshirts[i].price + ' euros</p><button type="button" id="button' + womenTshirts[i].ref + '">Ajouter au panier !</button></div>');
  }
 
+
+
+
+
+ /*console.log(womenSkirt[0].name);
+ for (var i = 0; i < womenSkirt.length; i++) {
+   $('#womenSkirt').append('<div class="article"><img src="' + womenSkirt[i].image + '" />\n<p class="articleName">' + womenSkirt[i].name + '</p>\n<p class="articleName">' + womenSkirt[i].price + ' euros</p><button type="button" id="button' + womenSkirt[i].ref + '">Mettre dans le panier !</button></div>');
+ }
+ console.log(womenSkirt[0].name);
+ for (var i = 0; i < womenSkirt.length; i++) {
+   $('#womenSkirt').append('<div class="article"><img src="' + womenSkirt[i].image + '" />\n<p class="articleName">' + womenSkirt[i].name + '</p>\n<p class="articleName">' + womenSkirt[i].price + ' euros</p><button type="button" id="button' + womenSkirt[i].ref + '">Mettre dans le panier !</button></div>');
+ }
+ console.log(womenSkirt[0].name);
+ for (var i = 0; i < womenSkirt.length; i++) {
+   $('#womenSkirt').append('<div class="article"><img src="' + womenSkirt[i].image + '" />\n<p class="articleName">' + womenSkirt[i].name + '</p>\n<p class="articleName">' + womenSkirt[i].price + ' euros</p><button type="button" id="button' + womenSkirt[i].ref + '">Mettre dans le panier !</button></div>');
+ }
+ console.log(womenSkirt[0].name);
+ for (var i = 0; i < womenSkirt.length; i++) {
+   $('#womenSkirt').append('<div class="article"><img src="' + womenSkirt[i].image + '" />\n<p class="articleName">' + womenSkirt[i].name + '</p>\n<p class="articleName">' + womenSkirt[i].price + ' euros</p><button type="button" id="button' + womenSkirt[i].ref + '">Mettre dans le panier !</button></div>');
+ }
+ console.log(womenSkirt[0].name);
+ for (var i = 0; i < womenSkirt.length; i++) {
+   $('#womenSkirt').append('<div class="article"><img src="' + womenSkirt[i].image + '" />\n<p class="articleName">' + womenSkirt[i].name + '</p>\n<p class="articleName">' + womenSkirt[i].price + ' euros</p><button type="button" id="button' + womenSkirt[i].ref + '">Mettre dans le panier !</button></div>');
+ }
+ console.log(womenSkirt[0].name);
+ for (var i = 0; i < womenSkirt.length; i++) {
+   $('#womenSkirt').append('<div class="article"><img src="' + womenSkirt[i].image + '" />\n<p class="articleName">' + womenSkirt[i].name + '</p>\n<p class="articleName">' + womenSkirt[i].price + ' euros</p><button type="button" id="button' + womenSkirt[i].ref + '">Mettre dans le panier !</button></div>');
+ }*/
   $('#navBarreWomen').click(function() {
     $('#women').show();
     $('.onlyWomen').hide();
